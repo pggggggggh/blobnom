b0e8f8f02ab3f2545b1ac71e0716593ef8e2a104
pggggggggh <pggggggggh@yonsei.ac.kr>
2024-11-26 22:31:05 +0900

    Server commit

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,11 +1,11 @@
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite'
 import react from '@vitejs/plugin-react-swc'
-import { TanStackRouterVite } from '@tanstack/router-plugin/vite'
+import {TanStackRouterVite} from '@tanstack/router-plugin/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-      react(),
-      TanStackRouterVite(),
-  ],
+    plugins: [
+        react(),
+        TanStackRouterVite(),
+    ],
 })
