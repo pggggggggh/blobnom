4b080b08240a6efb675037d68aa48ad1d01bb616
swoooon <swodori@gmail.com>
2024-11-29 11:40:02 +0900

    Create CreateRoom.tsx

---
+++ frontend/src/types/CreateRoom.tsx
@@ -0,0 +1,13 @@
+export interface CreateRoom {
+    owner: string;
+    edit_password: string;
+    handles: string;
+    title: string;
+    query: string;
+    size: number;
+    is_private: boolean;
+    max_players: number;
+    starts_at: string;
+    ends_at: string;
+    entry_pin?: string;
+}
