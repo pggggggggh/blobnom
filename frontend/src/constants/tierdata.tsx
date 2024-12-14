d1a5b9a6629d74b0e6f942675f6279da45ea0349
swoooon <swodori@gmail.com>
2024-12-14 15:58:10 +0900

    Chore: change tier name 'unrated' to 'ur'

--- frontend/src/constants/tierdata.tsx
+++ frontend/src/constants/tierdata.tsx
@@ -1,117 +1,117 @@
 import { ReactNode } from 'react';
-export type Tier = { value: number; label: string|ReactNode; short: string };
+export type Tier = { value: number; label: string | ReactNode; short: string };
 
 export const tiers: Tier[] = [
-    { value: 0, label: "Unrated", short: "unrated" },
-    { value: 1, label: "Bronze V", short: "b5" },
-    { value: 2, label: "Bronze IV", short: "b4" },
-    { value: 3, label: "Bronze III", short: "b3" },
-    { value: 4, label: "Bronze II", short: "b2" },
-    { value: 5, label: "Bronze I", short: "b1" },
-    { value: 6, label: "Silver V", short: "s5" },
-    { value: 7, label: "Silver IV", short: "s4" },
-    { value: 8, label: "Silver III", short: "s3" },
-    { value: 9, label: "Silver II", short: "s2" },
-    { value: 10, label: "Silver I", short: "s1" },
-    { value: 11, label: "Gold V", short: "g5" },
-    { value: 12, label: "Gold IV", short: "g4" },
-    { value: 13, label: "Gold III", short: "g3" },
-    { value: 14, label: "Gold II", short: "g2" },
-    { value: 15, label: "Gold I", short: "g1" },
-    { value: 16, label: "Platinum V", short: "p5" },
-    { value: 17, label: "Platinum IV", short: "p4" },
-    { value: 18, label: "Platinum III", short: "p3" },
-    { value: 19, label: "Platinum II", short: "p2" },
-    { value: 20, label: "Platinum I", short: "p1" },
-    { value: 21, label: "Diamond V", short: "d5" },
-    { value: 22, label: "Diamond IV", short: "d4" },
-    { value: 23, label: "Diamond III", short: "d3" },
-    { value: 24, label: "Diamond II", short: "d2" },
-    { value: 25, label: "Diamond I", short: "d1" },
-    { value: 26, label: "Ruby V", short: "r5" },
-    { value: 27, label: "Ruby IV", short: "r4" },
-    { value: 28, label: "Ruby III", short: "r3" },
-    { value: 29, label: "Ruby II", short: "r2" },
-    { value: 30, label: "Ruby I", short: "r1" },
+  { value: 0, label: "Unrated", short: "ur" },
+  { value: 1, label: "Bronze V", short: "b5" },
+  { value: 2, label: "Bronze IV", short: "b4" },
+  { value: 3, label: "Bronze III", short: "b3" },
+  { value: 4, label: "Bronze II", short: "b2" },
+  { value: 5, label: "Bronze I", short: "b1" },
+  { value: 6, label: "Silver V", short: "s5" },
+  { value: 7, label: "Silver IV", short: "s4" },
+  { value: 8, label: "Silver III", short: "s3" },
+  { value: 9, label: "Silver II", short: "s2" },
+  { value: 10, label: "Silver I", short: "s1" },
+  { value: 11, label: "Gold V", short: "g5" },
+  { value: 12, label: "Gold IV", short: "g4" },
+  { value: 13, label: "Gold III", short: "g3" },
+  { value: 14, label: "Gold II", short: "g2" },
+  { value: 15, label: "Gold I", short: "g1" },
+  { value: 16, label: "Platinum V", short: "p5" },
+  { value: 17, label: "Platinum IV", short: "p4" },
+  { value: 18, label: "Platinum III", short: "p3" },
+  { value: 19, label: "Platinum II", short: "p2" },
+  { value: 20, label: "Platinum I", short: "p1" },
+  { value: 21, label: "Diamond V", short: "d5" },
+  { value: 22, label: "Diamond IV", short: "d4" },
+  { value: 23, label: "Diamond III", short: "d3" },
+  { value: 24, label: "Diamond II", short: "d2" },
+  { value: 25, label: "Diamond I", short: "d1" },
+  { value: 26, label: "Ruby V", short: "r5" },
+  { value: 27, label: "Ruby IV", short: "r4" },
+  { value: 28, label: "Ruby III", short: "r3" },
+  { value: 29, label: "Ruby II", short: "r2" },
+  { value: 30, label: "Ruby I", short: "r1" },
 ];
 
 export const marks: Tier[] = [
-    {
-      value: 1,
-      label: (
-        <div style={{ display: 'flex', alignItems: 'center' }}>
-          <img
-            src="/images/bronze.png"
-            alt="Bronze 아이콘"
-            style={{ width: '17px', height: '22px' }}
-          />
-        </div>
-      ),
-      short: 'b5',
-    },
-    {
-      value: 6,
-      label: (
-        <div style={{ display: 'flex', alignItems: 'center' }}>
-          <img
-            src="/images/silver.png"
-            alt="Silver 아이콘"
-            style={{ width: '17px', height: '22px' }}
-          />
-        </div>
-      ),
-      short: 's5',
-    },
-    {
-      value: 11,
-      label: (
-        <div style={{ display: 'flex', alignItems: 'center' }}>
-          <img
-            src="/images/gold.png"
-            alt="Gold 아이콘"
-            style={{ width: '17px', height: '22px' }}
-          />
-        </div>
-      ),
-      short: 'g5',
-    },
-    {
-      value: 16,
-      label: (
-        <div style={{ display: 'flex', alignItems: 'center' }}>
-          <img
-            src="/images/platinum.png"
-            alt="Platinum 아이콘"
-            style={{ width: '17px', height: '22px' }}
-          />
-        </div>
-      ),
-      short: 'p5',
-    },
-    {
-      value: 21,
-      label: (
-        <div style={{ display: 'flex', alignItems: 'center' }}>
-          <img
-            src="/images/diamond.png"
-            alt="Diamond 아이콘"
-            style={{ width: '17px', height: '22px' }}
-          />
-        </div>
-      ),
-      short: 'd5',
-    },
-    {
-      value: 26,
-      label: (
-        <div style={{ display: 'flex', alignItems: 'center' }}>
-          <img
-            src="/images/ruby.png"
-            alt="Ruby 아이콘"
-            style={{ width: '17px', height: '22px' }}
-          />
-        </div>
-      ),
-      short: 'r5',
-    },
-  ];
+  {
+    value: 1,
+    label: (
+      <div style={{ display: 'flex', alignItems: 'center' }}>
+        <img
+          src="/images/bronze.png"
+          alt="Bronze 아이콘"
+          style={{ width: '17px', height: '22px' }}
+        />
+      </div>
+    ),
+    short: 'b5',
+  },
+  {
+    value: 6,
+    label: (
+      <div style={{ display: 'flex', alignItems: 'center' }}>
+        <img
+          src="/images/silver.png"
+          alt="Silver 아이콘"
+          style={{ width: '17px', height: '22px' }}
+        />
+      </div>
+    ),
+    short: 's5',
+  },
+  {
+    value: 11,
+    label: (
+      <div style={{ display: 'flex', alignItems: 'center' }}>
+        <img
+          src="/images/gold.png"
+          alt="Gold 아이콘"
+          style={{ width: '17px', height: '22px' }}
+        />
+      </div>
+    ),
+    short: 'g5',
+  },
+  {
+    value: 16,
+    label: (
+      <div style={{ display: 'flex', alignItems: 'center' }}>
+        <img
+          src="/images/platinum.png"
+          alt="Platinum 아이콘"
+          style={{ width: '17px', height: '22px' }}
+        />
+      </div>
+    ),
+    short: 'p5',
+  },
+  {
+    value: 21,
+    label: (
+      <div style={{ display: 'flex', alignItems: 'center' }}>
+        <img
+          src="/images/diamond.png"
+          alt="Diamond 아이콘"
+          style={{ width: '17px', height: '22px' }}
+        />
+      </div>
+    ),
+    short: 'd5',
+  },
+  {
+    value: 26,
+    label: (
+      <div style={{ display: 'flex', alignItems: 'center' }}>
+        <img
+          src="/images/ruby.png"
+          alt="Ruby 아이콘"
+          style={{ width: '17px', height: '22px' }}
+        />
+      </div>
+    ),
+    short: 'r5',
+  },
+];
