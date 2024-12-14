b20df7cf1665d5698aa89c7fb9a0576c15cb5bda
swoooon <swodori@gmail.com>
2024-12-14 17:39:30 +0900

    Fix: accept underscore on handle

--- frontend/src/components/Modals/RoomJoinModal.tsx
+++ frontend/src/components/Modals/RoomJoinModal.tsx
@@ -1,9 +1,9 @@
-import {Button, Image, Stack, TextInput} from "@mantine/core";
+import { Button, Image, Stack, TextInput } from "@mantine/core";
 import blobaww from "../../assets/blobaww.webp"
-import {useForm} from "@mantine/form";
-import {useJoinRoom} from "../../hooks/hooks.tsx";
+import { useForm } from "@mantine/form";
+import { useJoinRoom } from "../../hooks/hooks.tsx";
 
-const RoomJoinModal = ({roomId, is_private}: { roomId: number, is_private: boolean }) => {
+const RoomJoinModal = ({ roomId, is_private }: { roomId: number, is_private: boolean }) => {
     const mutation = useJoinRoom();
 
     const form = useForm({
@@ -14,7 +14,7 @@ const RoomJoinModal = ({roomId, is_private}: { roomId: number, is_private: boole
         },
         validate: {
             handle: (value) => {
-                if (!/^[a-zA-Z0-9]{1,50}$/.test(value)) {
+                if (!/^[a-zA-Z0-9_]{1,50}$/.test(value)) {
                     return "핸들 형식이 잘못되었습니다.";
                 }
                 return null;
@@ -24,14 +24,14 @@ const RoomJoinModal = ({roomId, is_private}: { roomId: number, is_private: boole
 
     const handleSubmit = (values: { handle: string; password: string; }) => {
         mutation.mutate(
-            {roomId, handle: values.handle, password: values.password},
+            { roomId, handle: values.handle, password: values.password },
         );
     };
 
     return (
         <form onSubmit={form.onSubmit(handleSubmit)}>
             <Stack align="center">
-                <Image w="64px" src={blobaww}/>
+                <Image w="64px" src={blobaww} />
                 <TextInput
                     {...form.getInputProps('handle')}
                     label="핸들을 입력해주세요!"
