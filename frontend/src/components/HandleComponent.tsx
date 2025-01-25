7e16f6f3b5f6e82af1d9ba6d517aac5ef7c9e69e
PGH <pggggggggh@yonsei.ac.kr>
2025-01-26 03:38:03 +0900

    feat: 비회원에게 경고문/툴팁 적용

--- frontend/src/components/HandleComponent.tsx
+++ frontend/src/components/HandleComponent.tsx
@@ -1,19 +1,22 @@
+import {Tooltip} from '@mantine/core';
+
 const HandleComponent = ({user}) => {
     return (
         <>
-            {
-                user.role ?
-                    <span
-                        className="">
-                        {user.handle}
-                    </span>
-                    :
-                    <span className="text-slate-400">
-                        {user.handle}
-                    </span>
-            }
+            {user.role ? (
+                <span>{user.handle}</span>
+            ) : (
+                <span className="inline-flex items-center">
+                    <Tooltip label="가입되지 않은 회원입니다." withArrow>
+                        <span className="flex items-center line-height-1">
+                            ⚠️
+                        </span>
+                    </Tooltip>
+                    <span className="text-slate-400">{user.handle}</span>
+                </span>
+            )}
         </>
-    )
-}
+    );
+};
 
-export default HandleComponent
+export default HandleComponent;
