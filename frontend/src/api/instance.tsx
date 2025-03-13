e2bd7f7b4e2ffaf83a0684b4423d791884d205a1
PGH <pggggggggh@yonsei.ac.kr>
2025-03-14 04:19:04 +0900

    fix: jwt token localstorage to httponly cookie

--- frontend/src/api/instance.tsx
+++ frontend/src/api/instance.tsx
@@ -3,31 +3,34 @@ import axios from 'axios';
 export const api = axios.create({
     baseURL: import.meta.env.VITE_API_URL,
     timeout: 50000,
+    withCredentials: true
 });
 
-api.interceptors.request.use(
-    (config) => {
-        const token = localStorage.getItem('accessToken');
-
-        if (token) {
-            config.headers.Authorization = `Bearer ${token}`;
-        }
-
-        return config;
-    },
-    (error) => {
-        return Promise.reject(error);
-    }
-);
+// api.interceptors.request.use(
+//     (config) => {
+//         const token = localStorage.getItem('accessToken');
+//         if (token) {
+//             config.headers.Authorization = `Bearer ${token}`;
+//         }
+//         return config;
+//     },
+//     (error) => {
+//         return Promise.reject(error);
+//     }
+// );
 
 api.interceptors.response.use(
     (response) => {
         return response;
     },
-    (error) => {
+    async (error) => {
         if (error.response && error.response.status === 401) {
-            localStorage.removeItem('accessToken'); // 기존 토큰 삭제
-            window.location.href = '/login'; // 로그인 페이지로 이동
+            try {
+                await api.post('/auth/logout');
+                window.location.href = '/login';
+            } catch (error) {
+                console.error('Failed to logout:', error);
+            }
         }
         return Promise.reject(error);
     }
