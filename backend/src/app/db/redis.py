6d729accfbf234d28f1214158f3cc86d9704adb6
pgh <pggggggggh@yonsei.ac.kr>
2026-01-01 21:11:04 +0900

    chore: restore redis connection

--- backend/src/app/db/redis.py
+++ backend/src/app/db/redis.py
@@ -11,8 +11,10 @@ redis_client = None
 async def create_redis_client():
     global redis_client
     try:
-        redis_client = redis.from_url(
-            os.environ["REDIS_URI"],
+        redis_client = redis.Redis(
+            host=os.environ.get("REDIS_URL"),
+            port=int(os.environ.get("REDIS_PORT")),
+            db=int(os.environ.get("REDIS_DBNO")),
             socket_timeout=2,
         )
         await redis_client.ping()
